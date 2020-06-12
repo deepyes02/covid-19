c8455d3cefd2976ea1a966613d1c2c7403eb0fd7
deepyes02 <deepyes@outlook.com>
2020-06-12 13:59:21 +0545

    first raw card displayed from infected

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import styles from './Cards.module.css';
 
 
-const Cards = () => {
+const Cards = (props) => {
+    console.log(props);
+    
+    
     return(
-        <h1>Cards</h1>
-    )
+        <div className={styles.container}>
+            <Grid container-spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent> 
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">Some Data</Typography>
+                        <Typography color="textSecondary">Some Date</Typography>
+                        <Typography variant="body2">Number of active cases of covid 19</Typography>
+                    </CardContent>
+
+                </Grid>
+
+            </Grid>
+
+
+        </div>
+    );
 }
 
 export default Cards;
