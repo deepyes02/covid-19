e81fd6e6e7ce88c98b78703458783bfad8feba74
deepyes02 <deepyes@outlook.com>
2020-07-01 12:00:14 +0545

    included build folder

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
 
 import {fetchData} from './api';
+import coronaimage from './images/image.png';
+import hikingLogo from './images/hiking-logo.png';
 
 class App extends React.Component {
     state = {
         data: {},
+        country: '',
     }
 
     async componentDidMount () {
@@ -18,15 +21,31 @@ class App extends React.Component {
         // console.log(fetchedData);
     }
 
+    handleCountryChange = async (country) =>{
+      const fetchedData = await fetchData(country);
+
+
+      // console.log(fetchedData); 
+      this.setState({data: fetchedData, country:country});
+      //fetch data
+      //set the state
+    } 
+
 
 
     render(){
-        const {data} = this.state;
+        const {data, country} = this.state;
         return (
             <div className={styles.container}>
+              <a href="http://www.hikingadventuretreks.com">
+              <img className={styles.logo} src={hikingLogo} alt="hiking-logo"/></a>
+              <img className={styles.image} src={coronaimage} alt="COVID-19"/>
                 <Cards data={data}/>
-                <CountryPicker/>
-                <Chart/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
+                <h3 className={styles.websiteTitle}>About Hiking Adventure Treks</h3>
+                <span> Covid-19 has affected many businesses across the world. Among them, we are also one. At this time, we are doing our best to make sure that our staff members are safe and looked after for. We are asking them to work from home. Travel industry is severely disrupted by the virus since most countries have still not opened their borders. We urge you to stay safe and keep in touch with us via our website and to know when we open our trekking and travel services to you.</span>
+                <a className={styles.button} href="http://www.hikingadventuretreks.com">Go to HomePage</a>
             </div>
         )
     }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import {fetchDailyData} from '../../api';
 import {Line, Bar} from 'react-chartjs-2';
 import styles from './Chart.module.css'; 
 
-const Chart = () => {
+const Chart = ({data: {confirmed, deaths, recovered}, country}) => {
     const [dailyData, setDailyData] = useState({});
 
     useEffect(() => {
@@ -14,14 +14,14 @@ const Chart = () => {
         // console.log(dailyData);
 
         fetchAPI();
-    });
+    }, []);
 
     const lineChart = (
         dailyData.length
         ? (
         <Line
         data = {{
-            labels: dailyData.map(({data}) => data),
+            labels: dailyData.map(({date}) => date),
             datasets: [{
                 data: dailyData.map(({confirmed}) => confirmed),
                 label: 'Infected',
@@ -30,7 +30,7 @@ const Chart = () => {
 
             }, {
                 data: dailyData.map(({deaths}) => deaths),
-                label: 'Infected',
+                label: 'Deaths',
                 borderColor: 'red',
                 backgroundColor: 'rgba(255,0,0,0.5)',
                 fill: true,
@@ -40,13 +40,35 @@ const Chart = () => {
         />) : null
     )
 
+    // console.log(confirmed, recovered, deaths);
 
+    const barChart = (
+      confirmed
+      ? (
+        <Bar 
+        data = {{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [{
+            label: 'People',
+            backgroundColor: [
+              'rgba(0,0,255,0.5)',
+              'rgba(0,255,0,0.5)',
+              'rgba(255,0,0,0.5)',
+            ],
+            data: [confirmed.value, recovered.value, deaths.value]
 
-
+          }],
+        }}
+        options = {{
+          legend: {display: false},
+          title: {display: true, text: `Current state in ${country}`,}
+        }}
+        />) : null
+    )
 
     return(
         <div className={styles.container}>
-            {lineChart}
+          {country? barChart : lineChart}
         </div>
     )
 }
