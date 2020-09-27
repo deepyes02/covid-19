a561ab1c8820777af8ecfe4f60203b29c1dd6d8a
deepyes02 <deepyes@outlook.com>
2020-09-27 11:39:33 +0545

    Updates

---
+++ precache-manifest.50b29efd535453c697209c16ad008142.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da28d6ee9e68e02a066c0fd1623c5117",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "dc9ed307df7d37f244c9",
+    "url": "/covid-19/static/css/main.085e41a9.chunk.css"
+  },
+  {
+    "revision": "936681ea540af8926140",
+    "url": "/covid-19/static/js/2.24848120.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.24848120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9ed307df7d37f244c9",
+    "url": "/covid-19/static/js/main.8f384138.chunk.js"
+  },
+  {
+    "revision": "42ecedb1c9ded48e58c5",
+    "url": "/covid-19/static/js/runtime-main.d3a4355d.js"
+  },
+  {
+    "revision": "8d46efaec1202884542e9f22956fface",
+    "url": "/covid-19/static/media/hiking-logo.8d46efae.png"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
